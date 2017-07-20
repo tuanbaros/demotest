3692d77371d73e41a6ea2b4dfef08863ce746454
tuannt <tuanbaros@yahoo.com.vn>
2017-07-20 15:20:18 +0700

    test

---
+++ app/src/test/java/com/simple/demotest/DemoTestSuite.java
@@ -0,0 +1,12 @@
+package com.simple.demotest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by FRAMGIA\nguyen.thanh.tuan on 7/21/17.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ MainActionTest.class, ExampleUnitTest.class })
+public class DemoTestSuite {
+}
